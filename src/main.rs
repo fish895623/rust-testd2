a93756455d3e81b4b793c7092547a3b9e29d2020
배성훈 <dan990429@gmail.com>
2023-06-21 12:28:18 +0900

    Add: get json

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
+use actix_web::{error, get, post, web, App, Error, HttpResponse, HttpServer, Responder};
+use futures::StreamExt;
+use serde;
 use std::env;
-
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello worldaaa!")
 }
 
+#[derive(serde::Serialize, serde::Deserialize)]
+struct MyObj {
+    name: String,
+    number: i32,
+}
+
+const MAX_SIZE: usize = 262_144;
+
+#[post("/hello")]
+async fn index_manual(mut payload: web::Payload) -> Result<HttpResponse, Error> {
+    let mut body = web::BytesMut::new();
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+        if (body.len() + chunk.len()) > MAX_SIZE {
+            return Err(error::ErrorBadRequest("overflow"));
+        }
+        body.extend_from_slice(&chunk);
+    }
+
+    let obj = serde_json::from_slice::<MyObj>(&body)?;
+    Ok(HttpResponse::Ok().json(obj))
+}
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
@@ -30,6 +53,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(index_manual)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", port))?
